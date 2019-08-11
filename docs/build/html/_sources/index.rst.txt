43729dce01b39bfe6c09a3d13becc63fe664bfce
Russ <russ@Eds-MacBook-Pro-254.local>
2019-08-11 07:45:26 -0500

    initial push commit for sphinx

---
+++ docs/build/html/_sources/index.rst.txt
@@ -0,0 +1,12 @@
+Welcome to Donor Data Pipeline Documentation!
+=====================================
+This is a place holder for the project description
+
+.. toctree::
+    :maxdepth: 2
+    :caption: Contents:
+
+    simple_test
+    
+
+
---
+++ test_module/simple_test.py
@@ -0,0 +1,16 @@
+"""
+Module simple_test
+================
+This Moduel is used to test the documentation.
+"""
+import math
+
+
+def multiply_nums(n1, n2):
+    """Function to multiplies two numbers.
+    n1 : Must be a numeric type
+    n2 : Must be a numeric type
+    """
+
+    result = n1 * n2
+    return result
