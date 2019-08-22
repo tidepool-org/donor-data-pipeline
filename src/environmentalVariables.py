b34093306bf4b3bd4f50aad2511103a8e883ccf5
Jason Meno <jasonmeno@msn.com>
2019-08-22 11:11:12 -0400

    Add docstring to environmentalVariables.py

--- src/environmentalVariables.py
+++ src/environmentalVariables.py
@@ -5,6 +5,7 @@ description: load environment variables
 version: 0.0.1
 created: 2018-02-21
 author: Ed Nykaza
+modified: 2019-08-15 (Jason Meno)
 dependencies:
     * .env file in same folder as this script (Tidepool see bigdata 1PWD)
 license: BSD-2-Clause
@@ -24,6 +25,28 @@ if isfile(dotenv_path):
 
 # %% define functions
 def get_environmental_variables(donorGroup):
+    """
+    Retrieves an email and password of a Tidepool bigdata donor group account
+    from the local environment.
+
+    Args:
+        donorGroup (str): The name of the donor group
+
+    Returns:
+        emailAddress (str): The email address of the donor group account
+        pswd (str): The password of the Tidepool donor group account
+
+    Raises:
+        KeyError if the variables are not found in the local environment
+
+    **This function only works if a local .env file is loaded into python**
+
+    This function is used by the following scripts:
+        get-donor-data/accept-new-donors-and-get-donor-list.py
+        get-donor-data/get_single_donor_metadata.py
+        get-donor-data/get_single_tidepool_dataset.py
+
+    """
     try:
         envEmailVariableName = "BIGDATA_" + donorGroup + "_EMAIL"
         emailAddress = os.environ[envEmailVariableName]
