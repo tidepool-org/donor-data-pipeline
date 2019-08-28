78ba10f69015daaab44a6491e7df0976168175b7
Jason Meno <jasonmeno@msn.com>
2019-08-28 14:58:39 -0400

    Add docstrings to get_single_tidepool_dataset.py

--- src/get-donor-data/get_single_tidepool_dataset.py
+++ src/get-donor-data/get_single_tidepool_dataset.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
-"""get_donor_data_and_metadata.py
-In the context of the big data donation
-project, this code grabs donor data and metadata.
+"""
+Download Tidepool Account Device Data
+=====================================
+:File: get_single_tidepool_dataset.py
+:Description: Downloads all a Tidepool account's device data
+:Version: 0.0.1
+:Created: 2019-05-26
+:Authors: Ed Nykaza (etn), Jason Meno (jam)
+:Last Modified: 2019-08-26 (jam)
+:Dependencies:
+    - .env
+    - environmentalVariables.py
+:License: BSD-2-Clause
 
-This code calls accept_new_donors_and_get_donor_list.py
-to get the most recent donor list
 """
 
 # %% REQUIRED LIBRARIES
@@ -24,85 +32,139 @@ if envPath not in sys.path:
 import environmentalVariables
 
 
-# %% USER INPUTS (choices to be made in order to run the code)
-codeDescription = "get donor metadata"
-parser = argparse.ArgumentParser(description=codeDescription)
+# %% Function Definitions
+def parse_arguments():
+    r"""Parses command line arguments
 
-parser.add_argument(
-    "-d",
-    "--date-stamp",
-    dest="date_stamp",
-    default=dt.datetime.now().strftime("%Y-%m-%d"),
-    help="date, in '%Y-%m-%d' format, of the date when " +
-    "donors were accepted"
-)
+    Parameters
+    ----------
+    None
 
-parser.add_argument(
-    "-w",
-    "--weeks-of-data",
-    dest="weeks_of_data",
-    default=52*10,
-    help="enter the number of weeks of data you want to download"
-)
+    Returns
+    -------
+    args : argparse.Namespace
+    A namespace of arguments parsed by the argparse package.
 
-parser.add_argument(
-    "-dg",
-    "--donor-group",
-    dest="donor_group",
-    default=np.nan,
-    help="name of the donor group in the tidepool .env file"
-)
+    This namespace includes the following:
+        **date_stamp** : str
+            A YYYY-MM-DD formatted datestamp
+        **weeks_of_data** : int
+            The number of weeks of data you want to download
+        **donor_group** : str
+            name of the donor group in the tidepool .env file
+        **userid_of_shared_user** : str
+            userid of account shared with the donor group or master account
+        **auth** : tuple
+            An ('email', 'password') string tuple of a Tidepool account
+        **email** : str
+            Master account email
+        **password** : str
+            Master account password
+        **data_path** : str
+            The absolute output path where the data is stored
 
-parser.add_argument(
-    "-u",
-    "--userid",
-    dest="userid_of_shared_user",
-    default=np.nan,
-    help="userid of account shared with the donor group or master account"
-)
+    Notes
+    -----
+    Called From:
+        - main
 
-parser.add_argument(
-    "-a",
-    "--auth",
-    dest="auth",
-    default=np.nan,
-    help="tuple that contains (email, password)"
-)
+    """
+    codeDescription = "get donor metadata"
+    parser = argparse.ArgumentParser(description=codeDescription)
 
-parser.add_argument(
-    "-e",
-    "--email",
-    dest="email",
-    default=np.nan,
-    help="email address of the master account"
-)
+    parser.add_argument(
+        "-d",
+        "--date-stamp",
+        dest="date_stamp",
+        default=dt.datetime.now().strftime("%Y-%m-%d"),
+        help="date, in '%Y-%m-%d' format, of the date when " +
+        "donors were accepted"
+    )
 
-parser.add_argument(
-    "-p",
-    "--password",
-    dest="password",
-    default=np.nan,
-    help="password of the master account"
-)
+    parser.add_argument(
+        "-w",
+        "--weeks-of-data",
+        dest="weeks_of_data",
+        default=52*10,
+        help="enter the number of weeks of data you want to download"
+    )
 
-parser.add_argument(
-    "-o",
-    "--output-data-path",
-    dest="data_path",
-    default=os.path.abspath(
-        os.path.join(
-            os.path.dirname(__file__), "..", "data"
-        )
-    ),
-    help="the output path where the data is stored"
-)
+    parser.add_argument(
+        "-dg",
+        "--donor-group",
+        dest="donor_group",
+        default=np.nan,
+        help="name of the donor group in the tidepool .env file"
+    )
 
-args = parser.parse_args()
+    parser.add_argument(
+        "-u",
+        "--userid",
+        dest="userid_of_shared_user",
+        default=np.nan,
+        help="userid of account shared with the donor group or master account"
+    )
+
+    parser.add_argument(
+        "-a",
+        "--auth",
+        dest="auth",
+        default=np.nan,
+        help="tuple that contains (email, password)"
+    )
+
+    parser.add_argument(
+        "-e",
+        "--email",
+        dest="email",
+        default=np.nan,
+        help="email address of the master account"
+    )
+
+    parser.add_argument(
+        "-p",
+        "--password",
+        dest="password",
+        default=np.nan,
+        help="password of the master account"
+    )
+
+    parser.add_argument(
+        "-o",
+        "--output-data-path",
+        dest="data_path",
+        default=os.path.abspath(
+            os.path.join(
+                os.path.dirname(__file__), "..", "data"
+            )
+        ),
+        help="the output path where the data is stored"
+    )
+
+    args = parser.parse_args()
+
+    return args
 
 
-# %% FUNCTIONS
 def make_folder_if_doesnt_exist(folder_paths):
-    ''' function requires a single path or a list of paths'''
+    r"""Makes folder directories from any folder path string or list of folder
+    path strings.
+
+    Parameters
+    ----------
+    folder_paths : str or list
+        The folder paths to be made
+
+    Returns
+    -------
+    None
+
+    Notes
+    -----
+    Called From:
+        - get_and_save_metadata
+
+    """
     if not isinstance(folder_paths, list):
         folder_paths = [folder_paths]
     for folder_path in folder_paths:
@@ -112,7 +174,32 @@ def make_folder_if_doesnt_exist(folder_paths):
 
 
 def get_data_api(userid, startDate, endDate, headers):
+    r"""Sends data request to Tidepool API
 
+    Parameters
+    ----------
+    userid : str
+        userid of account shared with the donor group or master account
+    startDate : datetime.datetime
+        The starting date for data to be gathered
+    endDate : datetime.datetime or pandas._libs.tslibs.timestamps.Timestamp
+        The ending date for data to be gathered (<= 1 year from startDate)
+
+
+    Returns
+    -------
+    df : pandas.DataFrame
+        A very large dataframe containg up to one year of Tidepool account
+        device data
+    endDate : pandas._libs.tslibs.timestamps.Timestamp
+        The next end date to be gathered, which is the startDate - 1 day
+
+    Notes
+    -----
+    Called From:
+        - get_data
+
+    """
     startDate = startDate.strftime("%Y-%m-%d") + "T00:00:00.000Z"
     endDate = endDate.strftime("%Y-%m-%d") + "T23:59:59.999Z"
 
@@ -150,6 +237,42 @@ def get_data(
     email=np.nan,
     password=np.nan,
 ):
+    r"""Logs into Tidepool account, requests one year of data at time, concats
+        this data together, and then logs out
+
+    Parameters
+    ----------
+    weeks_of_data : int
+        The number of weeks of data you want to download
+    donor_group : str
+        name of the donor group in the tidepool .env file
+    userid_of_shared_user : str
+        userid of account shared with the donor group or master account
+    auth : tuple
+        An ('email', 'password') string tuple of a Tidepool account
+    email : str
+        Master account email
+    password : str
+        Master account password
+
+
+    Returns
+    -------
+    df : pandas.DataFrame
+        A very large dataframe containg all concatenated Tidepool account
+        device data
+    userid_of_shared_user : str
+        userid of account shared with the donor group or master account
+
+    Notes
+    -----
+    Called From:
+        - get_and_save_dataset
+    Calls To:
+        - environmentalVariables.get_environmental_variables
+        - get_data_api
+
+    """
     # login
     if pd.notnull(donor_group):
         if donor_group == "bigdata":
@@ -244,17 +367,51 @@ def get_data(
     return df, userid_of_shared_user
 
 
-# %% START OF CODE
 def get_and_save_dataset(
-    date_stamp=args.date_stamp,
-    data_path=args.data_path,
-    weeks_of_data=args.weeks_of_data,
-    donor_group=args.donor_group,
-    userid_of_shared_user=args.userid_of_shared_user,
-    auth=args.auth,
-    email=args.email,
-    password=args.password
+    date_stamp=np.nan,
+    data_path=np.nan,
+    weeks_of_data=np.nan,
+    donor_group=np.nan,
+    userid_of_shared_user=np.nan,
+    auth=np.nan,
+    email=np.nan,
+    password=np.nan
 ):
+    r"""Calls other functions to create folders, gather data, and then saves it
+
+    Parameters
+    ----------
+    date_stamp : str
+        A YYYY-MM-DD formatted datestamp
+    data_path : str
+        The absolute output path where the data is stored
+    weeks_of_data : int
+        The number of weeks of data you want to download
+    donor_group : str
+        name of the donor group in the tidepool .env file
+    userid_of_shared_user : str
+        userid of account shared with the donor group or master account
+    auth : tuple
+        An ('email', 'password') string tuple of a Tidepool account
+    email : str
+        Master account email
+    password : str
+        Master account password
+
+
+    Returns
+    -------
+    None
+
+    Notes
+    -----
+    Called From:
+        - main
+    Calls To:
+        - make_folder_if_doesnt_exist
+        - get_data
+
+    """
     # create output folders if they don't exist
 
     phi_date_stamp = "PHI-" + date_stamp
@@ -285,7 +442,34 @@ def get_and_save_dataset(
     data.to_csv(dataset_output_path)
 
 
-if __name__ == "__main__":
+def main():
+    r"""Main function for get_single_tidepool_dataset.py
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+
+    Notes
+    -----
+    Called From:
+        - __main__
+
+    Calls To:
+        - parse_arguments
+        - get_and_save_dataset
+
+    Creates:
+        data/PHI-YYYY-MM-DD-donor-data/PHI-YYYY-MM-DD-csvData
+            PHI-<userid>.csv
+
+        This file can be up to several GB in size
+
+    """
+    args = parse_arguments()
     get_and_save_dataset(
         date_stamp=args.date_stamp,
         data_path=args.data_path,
@@ -296,3 +480,10 @@ if __name__ == "__main__":
         email=args.email,
         password=args.password
     )
+
+    return
+
+
+# %% Main Call
+if __name__ == "__main__":
+    main()
