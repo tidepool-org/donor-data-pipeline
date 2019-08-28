784c8d149c7bbeb83ef80e78a5f418beb073b24b
Jason Meno <jasonmeno@msn.com>
2019-08-28 14:14:42 -0400

    Add docstrings to accept_new_donors_and_get_donor_list.py

--- src/get-donor-data/get_single_donor_metadata.py
+++ src/get-donor-data/get_single_donor_metadata.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
-"""get_donor_data_and_metadata.py
-In the context of the big data donation
-project, this code grabs donor data and metadata.
+"""
+Get Donor Account Metadata
+===========================
+:File: get_donor_data_and_metadata.py
+:Description: Gets metadata for a Tidepool account
+:Version: 0.0.1
+:Created: 2019-05-26
+:Authors: Ed Nykaza (etn), Jason Meno (jam)
+:Last Modified: 2019-08-26 (jam)
+:Dependencies:
+    - .env
+    - environmentalVariables.py
+:License: BSD-2-Clause
 
-This code calls accept_new_donors_and_get_donor_list.py
-to get the most recent donor list
 """
 
 # %% REQUIRED LIBRARIES
@@ -24,77 +32,131 @@ if envPath not in sys.path:
 import environmentalVariables
 
 
-# %% USER INPUTS (choices to be made in order to run the code)
-codeDescription = "get donor metadata"
-parser = argparse.ArgumentParser(description=codeDescription)
+# %% Function Definitions
+def parse_arguments():
+    r"""Parses command line arguments
 
-parser.add_argument(
-    "-d",
-    "--date-stamp",
-    dest="date_stamp",
-    default=dt.datetime.now().strftime("%Y-%m-%d"),
-    help="date, in '%Y-%m-%d' format, of the date when " +
-    "donors were accepted"
-)
+    Parameters
+    ----------
+    None
 
-parser.add_argument(
-    "-dg",
-    "--donor-group",
-    dest="donor_group",
-    default=np.nan,
-    help="name of the donor group in the tidepool .env file"
-)
+    Returns
+    -------
+    args : argparse.Namespace
+    A namespace of arguments parsed by the argparse package.
 
-parser.add_argument(
-    "-u",
-    "--userid",
-    dest="userid_of_shared_user",
-    default=np.nan,
-    help="userid of account shared with the donor group or master account"
-)
+    This namespace includes the following:
+        **date_stamp** : str
+            A YYYY-MM-DD formatted datestamp
+        **donor_group** : str
+            name of the donor group in the tidepool .env file
+        **userid_of_shared_user** : str
+            userid of account shared with the donor group or master account
+        **auth** : tuple
+            An ('email', 'password') string tuple of a Tidepool account
+        **email** : str
+            Master account email
+        **password** : str
+            Master account password
+        **data_path** : str
+            The absolute output path where the data is stored
 
-parser.add_argument(
-    "-a",
-    "--auth",
-    dest="auth",
-    default=np.nan,
-    help="tuple that contains (email, password)"
-)
+    Notes
+    -----
+    Called From:
+        - main
 
-parser.add_argument(
-    "-e",
-    "--email",
-    dest="email",
-    default=np.nan,
-    help="email address of the master account"
-)
+    """
+    # USER INPUTS (choices to be made in order to run the code)
+    codeDescription = "get donor metadata"
+    parser = argparse.ArgumentParser(description=codeDescription)
 
-parser.add_argument(
-    "-p",
-    "--password",
-    dest="password",
-    default=np.nan,
-    help="password of the master account"
-)
+    parser.add_argument(
+        "-d",
+        "--date-stamp",
+        dest="date_stamp",
+        default=dt.datetime.now().strftime("%Y-%m-%d"),
+        help="date, in '%Y-%m-%d' format, of the date when " +
+        "donors were accepted"
+    )
 
-parser.add_argument(
-    "-o",
-    "--output-data-path",
-    dest="data_path",
-    default=os.path.abspath(
-        os.path.join(
-            os.path.dirname(__file__), "..", "data"
-        )
-    ),
-    help="the output path where the data is stored"
-)
+    parser.add_argument(
+        "-dg",
+        "--donor-group",
+        dest="donor_group",
+        default=np.nan,
+        help="name of the donor group in the tidepool .env file"
+    )
 
-args = parser.parse_args()
+    parser.add_argument(
+        "-u",
+        "--userid",
+        dest="userid_of_shared_user",
+        default=np.nan,
+        help="userid of account shared with the donor group or master account"
+    )
+
+    parser.add_argument(
+        "-a",
+        "--auth",
+        dest="auth",
+        default=np.nan,
+        help="tuple that contains (email, password)"
+    )
+
+    parser.add_argument(
+        "-e",
+        "--email",
+        dest="email",
+        default=np.nan,
+        help="email address of the master account"
+    )
+
+    parser.add_argument(
+        "-p",
+        "--password",
+        dest="password",
+        default=np.nan,
+        help="password of the master account"
+    )
+
+    parser.add_argument(
+        "-o",
+        "--output-data-path",
+        dest="data_path",
+        default=os.path.abspath(
+            os.path.join(
+                os.path.dirname(__file__), "..", "data"
+            )
+        ),
+        help="the output path where the data is stored"
+    )
+
+    args = parser.parse_args()
+
+    return args
 
 
 # %% FUNCTIONS
 def make_folder_if_doesnt_exist(folder_paths):
-    ''' function requires a single path or a list of paths'''
+    r"""Makes folder directories from any folder path string or list of folder
+    path strings.
+
+    Parameters
+    ----------
+    folder_paths : str or list
+        The folder paths to be made
+
+    Returns
+    -------
+    None
+
+    Notes
+    -----
+    Called From:
+        - get_and_save_metadata
+
+    """
     if not isinstance(folder_paths, list):
         folder_paths = [folder_paths]
     for folder_path in folder_paths:
@@ -110,6 +172,58 @@ def get_shared_metadata(
     email=np.nan,
     password=np.nan,
 ):
+    r"""Makes API calls to get account metadata
+
+    Parameters
+    ----------
+    donor_group : str
+        name of the donor group in the tidepool .env file
+    userid_of_shared_user : str
+        userid of account shared with the donor group or master account
+    auth : tuple
+        An ('email', 'password') string tuple of a Tidepool account
+    email : str
+        Master account email
+    password : str
+        Master account password
+
+    Returns
+    -------
+    df : pandas.DataFrame
+        A dataframe containing the following account metadata
+            userid : str
+                10 digit user id
+            diagnosisType : str
+                Diabetes diagnosis type (e.g. type1/type2/gestational)
+            diagnosisDate : str
+                Date of diabetes diagnosis in YYYY-MM-DD format
+            biologicalSex : str
+                Biological sex currently obtained from healthkit data
+            birthday : str
+                Birthdate in YYYY-MM-DD format
+            targetTimezone : str
+                Timezone Tidepool account is set to (e.g. US/Eastern)
+            targetDevices : array of str
+                an array of some device names found in the dataset
+                e.g. [‘omnipod’, ‘Dexcom’]
+            isOtherPerson : bool
+                True/False bool of whether account "isOtherPerson"
+            about : str
+                The custom text in the ‘about me’ section of the Tidepool
+                account
+        userid_of_shared_user : str
+            userid of account shared with the donor group or master account
+
+    Notes
+    -----
+    Called From:
+        - get_and_save_metadata
+    Calls To:
+        - environmentalVariables.get_environmental_variables
+
+    If no email/password is given, then this function will require user input
+
+    """
     # login
     if pd.notnull(donor_group):
         if donor_group == "bigdata":
@@ -203,14 +317,46 @@ def get_shared_metadata(
 
 # %% START OF CODE
 def get_and_save_metadata(
-    date_stamp=args.date_stamp,
-    data_path=args.data_path,
-    donor_group=args.donor_group,
-    userid_of_shared_user=args.userid_of_shared_user,
-    auth=args.auth,
-    email=args.email,
-    password=args.password
+    date_stamp=np.nan,
+    data_path=np.nan,
+    donor_group=np.nan,
+    userid_of_shared_user=np.nan,
+    auth=np.nan,
+    email=np.nan,
+    password=np.nan
 ):
+    r"""Calls other functions to create folders, gather data, and then saves it
+
+    Parameters
+    ----------
+    date_stamp : str
+        A YYYY-MM-DD formatted datestamp
+    donor_group : str
+        name of the donor group in the tidepool .env file
+    userid_of_shared_user : str
+        userid of account shared with the donor group or master account
+    auth : tuple
+        An ('email', 'password') string tuple of a Tidepool account
+    email : str
+        Master account email
+    password : str
+        Master account password
+    data_path : str
+        The absolute output path where the data is stored
+
+    Returns
+    -------
+    None
+
+    Notes
+    -----
+    Called From:
+        - :py:func:`main`
+    Calls To:
+        - make_folder_if_doesnt_exist
+        - get_shared_metadata
+
+    """
     # create output folders if they don't exist
     phi_date_stamp = "PHI-" + date_stamp
     donor_folder = os.path.join(data_path, phi_date_stamp + "-donor-data")
@@ -238,8 +384,31 @@ def get_and_save_metadata(
 
     meta_df.to_csv(meta_output_path)
 
+    return
 
-if __name__ == "__main__":
+
+def main():
+    r"""Main function for get_single_donor_metadata.py
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+
+    Notes
+    -----
+    Called From:
+        - __main__
+
+    Calls To:
+        - parse_arguments
+        - get_and_save_metadata
+
+    """
+    args = parse_arguments()
     get_and_save_metadata(
         date_stamp=args.date_stamp,
         data_path=args.data_path,
@@ -249,3 +418,9 @@ if __name__ == "__main__":
         email=args.email,
         password=args.password
     )
+
+    return
+
+
+if __name__ == "__main__":
+    main()
