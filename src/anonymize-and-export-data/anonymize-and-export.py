880fa05d934d3aa706587ef63b11322a6682e9da
Jason Meno <jasonmeno@msn.com>
2019-09-30 22:24:43 -0400

    Update anonymize-and-export.py
    Multiple changes including InPen data flattening, deduplication, and bolusSchedules

--- src/anonymize-and-export-data/anonymize-and-export.py
+++ src/anonymize-and-export-data/anonymize-and-export.py
@@ -23,7 +23,7 @@ import argparse
 import hashlib
 import ast
 import time
-
+import environmentalVariables
 
 # %% USER INPUTS
 codeDescription = "Anonymize and export Tidepool data"
@@ -59,8 +59,8 @@ parser.add_argument("-o",
                     dest="exportPath",
                     default=os.path.abspath(
                             os.path.join(
-                            os.path.dirname(__file__),
-                            "example-data", "export")),
+                            os.path.dirname('__file__'),
+                            "anonymized-export")),
                     help="the path where the data is exported")
 
 parser.add_argument("--merge-wizard-data",
@@ -286,11 +286,49 @@ def filterByApprovedDataFields(df, dataFieldsForExport):
 
 
 # %% CLEAN DATA FUNCTIONS
+def cleanDurationColumn(df):
+    r"""A cleaning function to move all non-float-type durations into their own
+    column
+
+    The physicalActivity datatype introduces json blobs into the duration field
+    which throws an error when searching for negative durations in the function
+    removeNegativeDurations
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        A very large dataframe contaning all Tidepool account device data
+
+    Returns
+    -------
+    df : pandas.DataFrame
+        A very large dataframe contaning all Tidepool account device data but
+        may contain a new "physicalActivity.info" column
+
+    Notes
+    -----
+    Called From:
+        - removeNegativeDurations
+
+    """
+    physicalActivityRows = df['type'] == 'physicalActivity'
+
+    if sum(physicalActivityRows) > 0:
+        df["physicalActivityInfo"] = np.nan
+        df.loc[physicalActivityRows, "physicalActivityInfo"] = df.loc[physicalActivityRows, 'duration']
+        df.loc[physicalActivityRows, 'duration'] = np.nan
+
+    return df
+
+
 def removeNegativeDurations(df):
     if "duration" in list(df):
-        nNegativeDurations = sum(df.duration < 0)
+        df = cleanDurationColumn(df)
+        nNegativeDurations = sum(df.duration.astype(float) < 0)
         if nNegativeDurations > 0:
-            df = df[~(df.duration < 0)]
+            df = df[~(df.duration.astype(float) < 0)]
+    else:
+        nNegativeDurations = np.nan
 
     return df
 
@@ -306,6 +344,15 @@ def removeInvalidCgmValues(df):
     nRemoved = nBefore - len(df)
 
     return df, nRemoved
+
+
+def createBolusSchedules(df):
+    if "bolus" in list(df):
+        bolus_schedule_locations = df["bolus"].str.contains("{") == True
+        df.loc[bolus_schedule_locations, "bolusSchedules"] = df.loc[bolus_schedule_locations, "bolus"]
+        df.loc[bolus_schedule_locations, "bolus"] = np.nan
+
+    return df
 
 
 def tslimCalibrationFix(df):
@@ -334,13 +381,161 @@ def tslimCalibrationFix(df):
     return df, nTandemAndPayloadCalReadings
 
 
+#%% CGM Specific Cleaning
+def add_uploadDateTime(df):
+    r"""Adds an "uploadTime" column to the dataframe and corrects missing
+    upload times to records from healthkit data
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        A very large dataframe contaning all Tidepool account device data
+
+
+    Returns
+    -------
+    df : pandas.DataFrame
+        The same dataframe as the input but with an added "uploadTime" column
+
+
+    Notes
+    -----
+    Called From:
+        - main
+
+    """
+    if "upload" in df.type.unique():
+        uploadTimes = pd.DataFrame(
+            df[df.type == "upload"].groupby("uploadId").time.describe()["top"]
+        )
+    else:
+        uploadTimes = pd.DataFrame(columns=["top"])
+    # if an upload does not have an upload date, then add one
+    # NOTE: this is a new fix introduced with healthkit data...we now have
+    # data that does not have an upload record
+    unique_uploadIds = set(df["uploadId"].unique())
+    unique_uploadRecords = set(
+        df.loc[df["type"] == "upload", "uploadId"].unique()
+    )
+    uploadIds_missing_uploadRecords = unique_uploadIds - unique_uploadRecords
+
+    for upId in uploadIds_missing_uploadRecords:
+        last_upload_time = df.loc[df["uploadId"] == upId, "time"].max()
+        uploadTimes.loc[upId, "top"] = last_upload_time
+
+    uploadTimes.reset_index(inplace=True)
+    uploadTimes.rename(
+        columns={
+            "top": "uploadTime",
+            "index": "uploadId"
+        },
+        inplace=True
+    )
+    df = pd.merge(df, uploadTimes, how='left', on='uploadId')
+    df["uploadTime"] = pd.to_datetime(df["uploadTime"])
+
+    return df
+
+
+def round_time(df, timeIntervalMinutes=5, timeField="time",
+               roundedTimeFieldName="roundedTime", startWithFirstRecord=True,
+               verbose=False):
+    '''
+    A general purpose round time function that rounds the "time"
+    field to nearest <timeIntervalMinutes> minutes
+    INPUTS:
+        * a dataframe (df) that contains a time field that you want to round
+        * timeIntervalMinutes (defaults to 5 minutes given that most cgms output every 5 minutes)
+        * timeField to round (defaults to the UTC time "time" field)
+        * roundedTimeFieldName is a user specified column name (defaults to roundedTime)
+        * startWithFirstRecord starts the rounding with the first record if True, and the last record if False (defaults to True)
+        * verbose specifies whether the extra columns used to make calculations are returned
+    '''
+
+    df.sort_values(by=timeField, ascending=startWithFirstRecord, inplace=True)
+    df.reset_index(drop=True, inplace=True)
+
+    # make sure the time field is in the right form
+    t = pd.to_datetime(df[timeField].astype('datetime64[ns]'))
+
+    # calculate the time between consecutive records
+    t_shift = pd.to_datetime(df[timeField].astype('datetime64[ns]').shift(1))
+    df["timeBetweenRecords"] = \
+        round((t - t_shift).dt.days*(86400/(60 * timeIntervalMinutes)) +
+              (t - t_shift).dt.seconds/(60 * timeIntervalMinutes)) * timeIntervalMinutes
+
+    # separate the data into chunks if timeBetweenRecords is greater than
+    # 2 times the <timeIntervalMinutes> minutes so the rounding process starts over
+    largeGaps = list(df.query("abs(timeBetweenRecords) > " + str(timeIntervalMinutes * 2)).index)
+    largeGaps.insert(0, 0)
+    largeGaps.append(len(df))
+
+    for gIndex in range(0, len(largeGaps) - 1):
+        chunk = t[largeGaps[gIndex]:largeGaps[gIndex+1]]
+        firstRecordChunk = t[largeGaps[gIndex]]
+
+        # calculate the time difference between each time record and the first record
+        df.loc[largeGaps[gIndex]:largeGaps[gIndex+1], "minutesFromFirstRecord"] = \
+            (chunk - firstRecordChunk).dt.days*(86400/(60)) + (chunk - firstRecordChunk).dt.seconds/(60)
+
+        # then round to the nearest X Minutes
+        # NOTE: the ".000001" ensures that mulitples of 2:30 always rounds up.
+        df.loc[largeGaps[gIndex]:largeGaps[gIndex+1], "roundedMinutesFromFirstRecord"] = \
+            round((df.loc[largeGaps[gIndex]:largeGaps[gIndex+1],
+                          "minutesFromFirstRecord"] / timeIntervalMinutes) + 0.000001) * (timeIntervalMinutes)
+
+        roundedFirstRecord = (firstRecordChunk + pd.Timedelta("1microseconds")).round(str(timeIntervalMinutes) + "min")
+        df.loc[largeGaps[gIndex]:largeGaps[gIndex+1], roundedTimeFieldName] = \
+            roundedFirstRecord + \
+            pd.to_timedelta(df.loc[largeGaps[gIndex]:largeGaps[gIndex+1],
+                                   "roundedMinutesFromFirstRecord"], unit="m")
+
+    # sort by time and drop fieldsfields
+    df.sort_values(by=timeField, ascending=startWithFirstRecord, inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    if verbose is False:
+        df.drop(columns=["timeBetweenRecords",
+                         "minutesFromFirstRecord",
+                         "roundedMinutesFromFirstRecord"], inplace=True)
+
+    return df
+
+
+def removeDuplicates(df, criteriaDF):
+    nBefore = len(df)
+    df = df.loc[~(df[criteriaDF].duplicated())]
+    df = df.reset_index(drop=True)
+    nDuplicatesRemoved = nBefore - len(df)
+
+    return df, nDuplicatesRemoved
+
+
+def removeCgmDuplicates(df, timeCriterion):
+    if timeCriterion in df:
+        df.sort_values(by=[timeCriterion, "uploadTime"],
+                       ascending=[False, False],
+                       inplace=True)
+        dfIsNull = df[df[timeCriterion].isnull()]
+        dfNotNull = df[df[timeCriterion].notnull()]
+        dfNotNull, nDuplicatesRemoved = removeDuplicates(dfNotNull, [timeCriterion, "value"])
+        df = pd.concat([dfIsNull, dfNotNull])
+        df.sort_values(by=[timeCriterion, "uploadTime"],
+                       ascending=[False, False],
+                       inplace=True)
+    else:
+        nDuplicatesRemoved = 0
+
+    return df, nDuplicatesRemoved
 # %% ANONYMIZE DATA FUNCTIONS
 def hashScheduleNames(df, salt, userID):
 
     scheduleNames = ["basalSchedules",
                      "bgTargets",
                      "carbRatios",
-                     "insulinSensitivities"]
+                     "insulinSensitivities",
+                     "bolusSchedules"]
+    
+    nameKeys = set()
 
     # loop through each of the scheduleNames that exist
     for scheduleName in scheduleNames:
@@ -359,10 +554,23 @@ def hashScheduleNames(df, salt, userID):
                 scheduleNameKeys = \
                     list(scheduleNameDataFrame[scheduleName]
                     [scheduleNameRow].keys())
+
+                # Some bolusSchedules have no keys, and this helps prevent the
+                # internal values from being hashed
+                if scheduleName == "bolusSchedules":
+                    scheduleNameKeys = set(nameKeys) & set(scheduleNameKeys)
+                else:
+                    nameKeys = nameKeys | set(scheduleNameKeys) - set(nameKeys)
+
                 # loop through each key and replace with hashed version
                 for scheduleNameKey in scheduleNameKeys:
+                    if(type(scheduleNameKey) == int):
+                        print(scheduleName + " scheduleNameKey: " + str(scheduleNameKey) + " (" + str(type(scheduleNameKey)) + ")")
+                    #print("args.salt: " + str(args.salt) + " (" + str(type(args.salt)) + ")")
+                    if(type(userID) == int):
+                        print("userID: " + str(userID) + " (" + str(type(userID)) + ")")
                     hashedScheduleName = \
-                    hashlib.sha256((scheduleNameKey + args.salt + userID).
+                    hashlib.sha256((str(scheduleNameKey) + str(args.salt) + str(userID)).
                                encode()).hexdigest()[0:8]
                     scheduleNameDataFrame[scheduleName][scheduleNameRow][hashedScheduleName] = \
                         scheduleNameDataFrame[scheduleName][scheduleNameRow].pop(scheduleNameKey)
@@ -667,7 +875,38 @@ def exportData(df, fileName, fileType, exportDirectory, mergeCalculatorData):
 
     return
 
-
+def flattenInpenData(df):
+    
+    bolus_df = df[df['type'] == 'bolus']
+    is_inpen = bolus_df.origin.str.contains('InPen') == True
+    inpen_idx = np.array(bolus_df.index[is_inpen])
+    
+    if len(inpen_idx) > 0:
+        #Separate inpen and data
+        inpen_df = bolus_df.loc[inpen_idx].copy()
+        holdout_df = df.drop(inpen_idx)
+        
+        #Convert dicts to dataframes
+        eval_df = inpen_df.payload.apply(ast.literal_eval)
+        dict_df = pd.DataFrame.from_records(eval_df.values, index=eval_df.index)
+        keep_columns = ['Duration Of Insulin Action In Seconds',
+                        'Insulin Concentration',
+                        'Insulin Name',
+                        'HKWasUserEntered']
+        
+        dict_df = dict_df[keep_columns].copy()
+        dict_df.columns = ['durationOfInsulinActionInSeconds',
+                           'insulinConcentration',
+                           'insulinName',
+                           'insulinManuallyEntered']
+        
+        dict_df['insulinManuallyEntered'] = dict_df['insulinManuallyEntered'].astype(bool)
+        
+        inpen_df[list(dict_df)] = dict_df
+        df = pd.concat([holdout_df, inpen_df], sort=True)
+    
+    return df
+    
 # %% LOAD DATA
 startTime = time.time()
 print("loading data...", end="")
@@ -676,6 +915,9 @@ print("loading data...", end="")
 data, userID = checkInputFile(args.inputFilePathAndName)
 print("done, took", round(time.time() - startTime, 1), "seconds")
 
+# %% Flatten Inpen Data
+if 'bolus' in data['type'].unique():
+    data = flattenInpenData(data)
 
 # %% FILTER DATA
 startTime = time.time()
@@ -702,13 +944,39 @@ print("cleaning data...", end="")
 # remove negative durations
 data = removeNegativeDurations(data)
 
+# Tslim calibration bug fix
+data, numberOfTandemAndPayloadCalReadings = tslimCalibrationFix(data)
+
 # get rid of cgm values too low/high (< 38 & > 402 mg/dL)
 data, numberOfInvalidCgmValues = removeInvalidCgmValues(data)
 
-# Tslim calibration bug fix
-data, numberOfTandemAndPayloadCalReadings = tslimCalibrationFix(data)
-print("done, took", round(time.time() - startTime, 1), "seconds")
+data = add_uploadDateTime(data)
 
+# Separate and Drop CGM
+cgm_df = data[data['type']=='cbg'].copy()
+data = data[~(data['type']=='cbg')].copy()
+
+cgm_df, numberOfInvalidCgmValues = removeInvalidCgmValues(cgm_df)
+cgm_df, nDuplicatesRemovedDeviceTime = removeCgmDuplicates(cgm_df, "deviceTime")
+cgm_df, nDuplicatesRemovedUtcTime = removeCgmDuplicates(cgm_df, "time")
+
+# round time to the nearest 5 minutes
+cgm_df = round_time(
+    cgm_df,
+    timeIntervalMinutes=5,
+    timeField="time",
+    roundedTimeFieldName="roundedTime",
+    verbose=False
+)
+
+# get rid of duplicates that have the same "roundedTime"
+cgm_df, nDuplicatesRemovedRoundedTime = removeDuplicates(cgm_df, "roundedTime")
+
+# Add cleaned CGM data back into dataframe
+data = pd.concat([data, cgm_df], sort=False)
+data.drop(columns=['roundedTime', 'uploadTime'], inplace=True)
+
+print("done, took", round(time.time() - startTime, 1), "seconds")
 
 # %% ANONYMIZE DATA
 
@@ -717,7 +985,7 @@ if "t" in args.anonymize.lower():
     print("anonymzing data...", end="")
     # remove manufacturer from annotations.code
     data = removeManufacturersFromAnnotationsCode(data)
-
+    data = createBolusSchedules(data)
     # hash the required data fields
     data = anonymizeData(data, anonymizeFields, args.salt, userID)
     hashID = hashUserId(userID, args.salt)
